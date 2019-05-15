56bb2f894a3888232c110f2fa1bd81afdc82cdcf
Fernando Serrano <fernandojsg@gmail.com>
2019-05-15 22:57:25 +0200

    Added getComponent/getMutableComponent

--- src/Entity.js
+++ src/Entity.js
@@ -1,28 +1,121 @@
+import Query from "./Query.js";
+
+const DEBUG = true;
+
+// @todo reset it by world?
+var nextId = 0;
+
+const proxyHandler = {
+  set(target, prop) {
+    throw new Error(
+      `Tried to write to "${target.constructor.name}#${String(
+        prop
+      )}" on immutable component. Use .getMutableComponent() to write to a component.`
+    );
+  }
+};
+
+const proxyMap = new WeakMap();
+
+function wrapImmutableComponent(T, component) {
+  if (component === undefined) {
+    return undefined;
+  }
+
+  let wrappedComponent = proxyMap.get(component);
+
+  if (!wrappedComponent) {
+    wrappedComponent = new Proxy(component, proxyHandler);
+    proxyMap.set(component, wrappedComponent);
+  }
+
+  return wrappedComponent;
+}
+
 export default class Entity {
   constructor(manager) {
     this._manager = manager || null;
+
+    // Unique ID for this entity
     this.id = nextId++;
+
+    // List of components types the entity has
     this._Components = [];
+
+    // Instance of the components
+    this._ComponentsMap = {};
+
+    // List of tags this entity has
     this._tags = [];
+
+    // Queries where the entity is added
+    this.queries = [];
   }
 
-  //---------------------------------------------------------------------------
   // COMPONENTS
-  //---------------------------------------------------------------------------
+
+  /**
+   * Return an immutable reference of a component
+   * Note: A proxy will be used on debug mode, and it will just affect
+   *       the first level attributes on the object, it won't work recursively.
+   * @param {Component} Type of component to get
+   * @return {Component} Immutable component reference
+   */
+  getComponent(Component) {
+    var component = this._ComponentsMap[Component.name];
+    if (DEBUG) return wrapImmutableComponent(Component, component);
+    return component;
+  }
+
+  /**
+   * Return a mutable reference of a component.
+   * @param {Component} Type of component to get
+   * @return {Component} Mutable component reference
+   */
+  getMutableComponent(Component) {
+    var component = this._ComponentsMap[Component.name];
+    for (var i = 0; i < this.queries.length; i++) {
+      var query = this.queries[i];
+      query.eventDispatcher.dispatchEvent(
+        Query.prototype.COMPONENT_CHANGED,
+        this,
+        component
+      );
+    }
+    return component;
+  }
+
+  /**
+   * Add a component to the entity
+   * @param {Component} Component to add to this entity
+   * @param {Object} Optional values to replace the default attributes on the component
+   */
   addComponent(Component, values) {
     this._manager.entityAddComponent(this, Component, values);
     return this;
   }
 
+  /**
+   * Remove a component from the entity
+   * @param {Component} Component to remove from the entity
+   */
   removeComponent(Component) {
     this._manager.entityRemoveComponent(this, Component);
     return this;
   }
 
+  /**
+   * Check if the entity has a component
+   * @param {Component} Component to check
+   */
   hasComponent(Component) {
     return !!~this._Components.indexOf(Component);
   }
 
+  /**
+   * Check if the entity has a list of components
+   * @param {Array(Component)} Components to check
+   */
   hasAllComponents(Components) {
     var result = true;
 
@@ -33,31 +126,46 @@ export default class Entity {
     return result;
   }
 
+  /**
+   * Remove all the components from the entity
+   */
   removeAllComponents() {
     return this._manager.entityRemoveAllComponents(this);
   }
 
-  //---------------------------------------------------------------------------
   // TAGS
-  //---------------------------------------------------------------------------
 
+  /**
+   * Check if the entity has a tag
+   * @param {String} tag Tag to check
+   */
   hasTag(tag) {
     return !!~this._tags.indexOf(tag);
   }
 
+  /**
+   * Add a tag to this entity
+   * @param {String} tag Tag to add to this entity
+   */
   addTag(tag) {
     this._manager.entityAddTag(this, tag);
     return this;
   }
 
+  /**
+   * Remove a tag from the entity
+   * @param {String} tag Tag to remove from the entity
+   */
   removeTag(tag) {
     this._manager.entityRemoveTag(this, tag);
     return this;
   }
 
-  //---------------------------------------------------------------------------
   // EXTRAS
-  //---------------------------------------------------------------------------
+  
+  /**
+   * Initialize the entity. To be used when returning an entity to the pool
+   */
   __init() {
     this.id = nextId++;
     this._manager = null;
@@ -65,13 +173,10 @@ export default class Entity {
     this._tags.length = 0;
   }
 
-  trigger(eventName, option) {
-    this._manager.trigger(eventName, this, option);
-  }
-
+  /**
+   * Dispose the entity from the manager
+   */
   dispose() {
     return this._manager.removeEntity(this);
   }
 }
-
-var nextId = 0;
