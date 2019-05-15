9ad07ade94203a4ccfe0b82b49c514eeff2b859c
Fernando Serrano <fernandojsg@gmail.com>
2019-05-15 22:50:22 +0200

    Change Group to Query

---
+++ src/Query.js
@@ -0,0 +1,37 @@
+import EventDispatcher from "./EventDispatcher.js";
+import { queryKey } from "./Utils.js";
+
+export default class Query {
+  /**
+   * @param {Array(Component)} Components List of types of components to query
+   */
+  constructor(Components, manager) {
+    this.Components = Components;
+    this.entities = [];
+    this.eventDispatcher = new EventDispatcher();
+
+    this.key = queryKey(Components);
+
+    // Fill the query with the existing entities
+    for (var i = 0; i < manager._entities.length; i++) {
+      var entity = manager._entities[i];
+      if (entity.hasAllComponents(Components)) {
+        this.entities.push(entity);
+      }
+    }
+  }
+
+  /**
+   * Return stats for this query
+   */
+  stats() {
+    return {
+      numComponents: this.Components.length,
+      numEntities: this.entities.length
+    };
+  }
+}
+
+Query.prototype.ENTITY_ADDED = "Query#ENTITY_ADDED";
+Query.prototype.ENTITY_REMOVED = "Query#ENTITY_REMOVED";
+Query.prototype.COMPONENT_CHANGED = "Query#COMPONENT_CHANGED";
---
+++ src/QueryManager.js
@@ -0,0 +1,92 @@
+import Query from "./Query.js";
+import { queryKey } from "./Utils.js";
+
+export default class QueryManager {
+  constructor(manager) {
+    this._manager = manager;
+
+    // Queries indexed by a unique identifier for the components it has
+    this._queries = {};
+  }
+
+  /**
+   * Callback when a component is added to an entity
+   * @param {Entity} entity Entity that just got the new component
+   * @param {Component} Component Component added to the entity
+   */
+  onEntityAdded(entity, Component) {
+    // @todo Use bitmask for checking components?
+
+    // Check each indexed query to see if we need to add this entity to the list
+    for (var queryName in this._queries) {
+      var query = this._queries[queryName];
+
+      // Add the entity only if:
+      // Component is in the query
+      // and Entity has ALL the components of the query
+      // and Entity is not already in the query
+      if (
+        !~query.Components.indexOf(Component) ||
+        !entity.hasAllComponents(query.Components) ||
+        ~query.entities.indexOf(entity)
+      )
+        continue;
+
+      query.eventDispatcher.dispatchEvent(Query.prototype.ENTITY_ADDED, entity);
+
+      entity.queries.push(query);
+      query.entities.push(entity);
+    }
+  }
+
+  /**
+   * Callback when a component is removed from an entity
+   * @param {Entity} entity Entity to remove the component from
+   * @param {Component} Component Component to remove from the entity
+   */
+  onEntityRemoved(entity, Component) {
+    for (var queryName in this._queries) {
+      var query = this._queries[queryName];
+
+      if (!~query.Components.indexOf(Component)) continue;
+      if (!entity.hasAllComponents(query.Components)) continue;
+
+      var index = query.entities.indexOf(entity);
+      if (~index) {
+        query.entities.splice(index, 1);
+
+        index = entity.queries.indexOf(query);
+        entity.queries.splice(index, 1);
+
+        query.eventDispatcher.dispatchEvent(
+          Query.prototype.ENTITY_REMOVED,
+          entity
+        );
+      }
+    }
+  }
+
+  /**
+   * Get a query for the specified components
+   * @param {Component} Components Components that the query should have
+   */
+  getQuery(Components) {
+    var key = queryKey(Components);
+    var query = this._queries[key];
+    if (!query) {
+      this._queries[key] = query = new Query(Components, this._manager);
+    }
+    return query;
+  }
+
+  /**
+   * Return some stats from this class
+   */
+  stats() {
+    var stats = {};
+    for (var queryName in this._queries) {
+      stats[queryName] = this._queries[queryName].stats();
+    }
+    return stats;
+  }
+}
