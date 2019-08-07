aeef4a20e7428491fb80df1a4d50fe39adff7331
Fernando Serrano <fernandojsg@gmail.com>
2019-08-07 12:33:54 +0200

    Add array of vector3 test of example

--- test/unit/createcomponent.test.js
+++ test/unit/createcomponent.test.js
@@ -133,3 +133,84 @@ test("copy", t => {
   t.is(c2.value, 10);
   t.deepEqual(c2.array, [1, 2, 3]);
 });
+
+test("Array of vector3 type", t => {
+  var Vector3Array = createType({
+    create: defaultValue => {
+      var v = [];
+      if (typeof defaultValue !== "undefined") {
+        for (var i = 0; i < defaultValue.length; i++) {
+          var value = defaultValue[i];
+          v.push(new Vector3(value.x, value.y, value.z));
+        }
+      }
+      return v;
+    },
+    reset: (src, key, defaultValue) => {
+      if (typeof defaultValue !== "undefined") {
+        for (var i = 0; i < defaultValue.length; i++) {
+          if (i < src[key].length) {
+            src[key][i].copy(defaultValue[i]);
+          } else {
+            var value = defaultValue[i];
+            src[key].push(new Vector3(value.x, value.y, value.z));
+          }
+        }
+
+        // Remove if the number of elements on the default value is lower than the current value
+        var diff = src[key].length - defaultValue.length;
+        src[key].splice(defaultValue.length - diff + 1, diff);
+      } else {
+        src[key].length = 0;
+      }
+    },
+    clear: (src, key) => {
+      src[key].length = 0;
+    }
+  });
+
+  var schema = {
+    v3array: {
+      default: [new Vector3(1, 2, 3), new Vector3(4, 5, 6)],
+      type: Vector3Array
+    }
+  };
+
+  var ComponentA = createComponent(schema, "ComponentA");
+
+  var c1 = new ComponentA();
+  var c2 = new ComponentA();
+
+  t.is(c1.v3array.length, 2);
+  t.deepEqual(c1.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+  t.is(c2.v3array.length, 2);
+  t.deepEqual(c2.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+
+  c1.v3array[0].x = 10;
+  c1.v3array[1].y = 20;
+
+  t.deepEqual(c1.v3array, [new Vector3(10, 2, 3), new Vector3(4, 20, 6)]);
+  t.deepEqual(c2.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+
+  c1.reset();
+
+  t.deepEqual(c1.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+  t.deepEqual(c2.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+
+  c1.v3array.push(new Vector3(7, 8, 9));
+  t.deepEqual(c1.v3array, [
+    new Vector3(1, 2, 3),
+    new Vector3(4, 5, 6),
+    new Vector3(7, 8, 9)
+  ]);
+
+  c1.reset();
+  t.deepEqual(c1.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+
+  c1.v3array.splice(1, 1);
+  c1.reset();
+  t.deepEqual(c1.v3array, [new Vector3(1, 2, 3), new Vector3(4, 5, 6)]);
+
+  c1.clear();
+  t.is(c1.v3array.length, 0);
+});
