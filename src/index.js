c43562d9b90a808e7fd2285d2f90918dc415ed94
Fernando Serrano <fernandojsg@gmail.com>
2019-09-18 14:33:19 +0200

    Fix #76 s/createComponent/createComponentClass

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export { World } from "./World.js";
 export { System, Not } from "./System.js";
 export { Component } from "./Component.js";
 export { TagComponent } from "./TagComponent.js";
-export { createComponent } from "./CreateComponent.js";
+export { createComponentClass } from "./CreateComponentClass.js";
 export { createType } from "./CreateType.js";
 export { Types } from "./StandardTypes.js";
