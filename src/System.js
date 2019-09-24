c1e1de32aafeec501df7b616b7c82054a698a761
Fernando Serrano <fernandojsg@gmail.com>
2019-09-25 00:30:40 +0200

    Remove events

--- src/System.js
+++ src/System.js
@@ -22,9 +22,6 @@ export class System {
     this._queries = {};
     this.queries = {};
 
-    this._events = {};
-    this.events = {};
-
     this.priority = 0;
 
     // Used for stats
@@ -134,60 +131,8 @@ export class System {
             }
           });
         }
-
-        /*
-        if (queryConfig.events) {
-          this.events[name] = {};
-          let events = this.events[name];
-          for (let eventName in queryConfig.events) {
-            let event = queryConfig.events[eventName];
-            events[eventName] = [];
-
-            const eventMapping = {
-              EntityAdded: Query.prototype.ENTITY_ADDED,
-              EntityRemoved: Query.prototype.ENTITY_REMOVED,
-              EntityChanged: Query.prototype.COMPONENT_CHANGED // Query.prototype.ENTITY_CHANGED
-            };
-
-            if (eventMapping[event.event]) {
-              query.eventDispatcher.addEventListener(
-                eventMapping[event.event],
-                entity => {
-                  // @fixme A lot of overhead?
-                  if (events[eventName].indexOf(entity) === -1)
-                    events[eventName].push(entity);
-                }
-              );
-              if (event.event === "EntityChanged") {
-                query.reactive = true;
-              }
-            } else if (event.event === "ComponentChanged") {
-              query.reactive = true;
-              query.eventDispatcher.addEventListener(
-                Query.prototype.COMPONENT_CHANGED,
-                (entity, component) => {
-                  if (event.components.indexOf(component.constructor) !== -1) {
-                    events[eventName].push(entity);
-                  }
-                }
-              );
-            }
-          }
-        }
-        */
       }
     }
-    /*
-    if (this.config.events) {
-      for (let eventName in this.config.events) {
-        var event = this.config.events[eventName];
-        this.events[eventName] = [];
-        this.world.addEventListener(event, data => {
-          this.events[eventName].push(data);
-        });
-      }
-    }
-    */
   }
 
   stop() {
@@ -208,17 +153,11 @@ export class System {
         if (Array.isArray(query.changed)) {
           query.changed.length = 0;
         } else {
-          for (name in query.changed) {
+          for (let name in query.changed) {
             query.changed[name].length = 0;
           }
         }
       }
-      // @todo add changed
-    }
-
-    // @question Standard events TBD?
-    for (var name in this.events) {
-      this.events[name].length = 0;
     }
   }
 
@@ -228,17 +167,18 @@ export class System {
       enabled: this.enabled,
       executeTime: this.executeTime,
       priority: this.priority,
-      queries: {},
-      events: {}
+      queries: {}
     };
 
     if (this.config) {
       var queries = this.queries;
       for (let queryName in queries) {
+        /*
         let query = queries[queryName];
         json.queries[queryName] = {
           key: this._queries[queryName].key
         };
+        /*
         if (query.events) {
           let events = (json.queries[queryName]["events"] = {});
           for (let eventName in query.events) {
@@ -252,13 +192,7 @@ export class System {
             }
           }
         }
-      }
-
-      let events = this.config.events;
-      for (let eventName in events) {
-        json.events[eventName] = {
-          eventName: events[eventName]
-        };
+        */
       }
     }
 
