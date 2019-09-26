c0a218699cfa1659a63d419887946a10a0191bbf
Fernando Serrano <fernandojsg@gmail.com>
2019-09-27 00:14:40 +0200

    Fixes #42 Add minified build

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,36 @@
 import json from "rollup-plugin-json";
+import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/index.js",
-  plugins: [json({ exclude: ["node_modules/**"] })],
-  output: [
-    {
-      format: "umd",
-      name: "ECSY",
-      noConflict: true,
-      file: "build/ecsy.js",
-      indent: "\t"
-    },
-    {
-      format: "es",
-      file: "build/ecsy.module.js",
-      indent: "\t"
-    }
-  ]
-};
+export default [
+  {
+    input: "src/index.js",
+    plugins: [json({ exclude: ["node_modules/**"] })],
+    output: [
+      {
+        format: "umd",
+        name: "ECSY",
+        noConflict: true,
+        file: "build/ecsy.js",
+        indent: "\t"
+      },
+      {
+        format: "es",
+        file: "build/ecsy.module.js",
+        indent: "\t"
+      }
+    ]
+  },
+  {
+    input: "src/index.js",
+    plugins: [json({ exclude: ["node_modules/**"] }), terser()],
+    output: [
+      {
+        format: "umd",
+        name: "ECSY",
+        noConflict: true,
+        file: "build/ecsy.min.js",
+        indent: "\t"
+      }
+    ]
+  }
+];
