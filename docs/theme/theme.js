2b1ae561e6404ae41545dff859509c8856a294e3
Brian Peiris <brianpeiris@gmail.com>
2019-09-30 20:33:24 -0700

    remove unused helper

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,11 +1,5 @@
 const Handlebars = require("handlebars");
 
-Handlebars.registerHelper("ifNoKind", function(options) {
-  if (!this.kindString) {
-    return options.fn(this);
-  }
-});
-
 const MarkdownTheme = require("typedoc-plugin-markdown/dist/theme").default;
 class CustomMarkdownTheme extends MarkdownTheme {
   constructor(renderer, basePath) {
