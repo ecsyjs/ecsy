d6333d9b6a3ae949ce3e643154cc5f61379e1021
Fernando Serrano <fernandojsg@gmail.com>
2019-12-03 12:30:32 +0100

    Fix clearevents just on executed systems

--- src/SystemManager.js
+++ src/SystemManager.js
@@ -50,9 +50,9 @@ export class SystemManager {
         let startTime = performance.now();
         system.execute(delta, time);
         system.executeTime = performance.now() - startTime;
+        this.lastExecutedSystem = system;
+        system.clearEvents();
       }
-      this.lastExecutedSystem = system;
-      system.clearEvents();
     }
   }
 
