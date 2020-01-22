9833bbdfb18226790dd5c686087562d967cd3a0a
Ilya Verpovskiy <iliaverpa@gmail.com>
2020-01-23 02:53:36 +0300

    fix Component interface export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { World } from './world';
 export { System } from './system';
 export { Not } from './not';
-export { Component } from './component.interface';
+export * from './component.interface'; // Component
 export { SystemStateComponent }  from './entity';
 export { TagComponent } from './tag-component';
 export { createComponentClass, createType, standardTypes } from './schema';
