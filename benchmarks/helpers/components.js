405477e1d909a695c4f6c50e0560f07323a29a19
Fernando Serrano <fernandojsg@gmail.com>
2020-05-29 23:09:04 +0200

    More benchmarks

--- benchmarks/helpers/components.js
+++ benchmarks/helpers/components.js
@@ -36,18 +36,6 @@ const Vector3Type = createType({
   clone: cloneClonable
 });
 
-class Vector3 {
-  constructor(x = 0, y = 0, z = 0) {
-    this.set(x, y, z);
-  }
-
-  set(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-}
-
 export class TagComponentA extends TagComponent {}
 export class TagComponentB extends TagComponent {}
 export class TagComponentC extends TagComponent {}
