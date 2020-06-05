612609385fc3366f37b0643c6702be17de39a9a8
Robert Long <robert@robertlong.me>
2020-06-04 23:02:19 -0700

    Use Vector3 type in benchmark to match original implementation.

--- benchmarks/helpers/components.js
+++ benchmarks/helpers/components.js
@@ -1,8 +1,13 @@
 import { TagComponent } from "../../src/index";
 import { Component } from "../../src/Component";
-import { Types } from "../../src/Types";
+import {
+  Types,
+  createType,
+  copyCopyable,
+  cloneClonable
+} from "../../src/Types";
 
-class Vector3 {
+export class Vector3 {
   constructor(x = 0, y = 0, z = 0) {
     this.set(x, y, z);
   }
@@ -12,7 +17,24 @@ class Vector3 {
     this.y = y;
     this.z = z;
   }
+
+  copy(source) {
+    this.x = source.x;
+    this.y = source.y;
+    return this;
+  }
+
+  clone() {
+    return new Vector3(this.x, this.y, this.z);
+  }
 }
+
+const Vector3Type = createType({
+  name: "Vector3",
+  default: new Vector3(),
+  copy: copyCopyable,
+  clone: cloneClonable
+});
 
 export class TagComponentA extends TagComponent {}
 export class TagComponentB extends TagComponent {}
@@ -33,7 +55,7 @@ export class Component3 extends Component {}
 Component3.schema = {
   attr: { type: Types.Number },
   attr2: { type: Types.Number },
-  attr3: { type: Types.Array }
+  attr3: { type: Vector3Type }
 };
 /*
 export class Component3NoReset extends Component {
