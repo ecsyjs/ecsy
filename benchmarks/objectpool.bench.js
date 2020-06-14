bd3c631279f1dff0f9e5feec4ea77e7d7808ac05
Fernando Serrano <fernandojsg@gmail.com>
2020-06-14 04:27:55 +0200

    Added acquiring and returning

--- benchmarks/objectpool.bench.js
+++ benchmarks/objectpool.bench.js
@@ -15,5 +15,42 @@ export function init(benchmarks) {
       execute: () => {
         new ObjectPool(Component3, 100000);
       }
+    })
+    .add({
+      name: "acquiring 100k. ObjectPool(Component1, 100k)",
+      prepare: ctx => {
+        ctx.pool = new ObjectPool(Component3, 100000);
+      },
+      execute: ctx => {
+        for (let i = 0; i < 100000; i++) {
+          ctx.pool.acquire();
+        }
+      }
+    })
+    .add({
+      name: "acquiring 100k. ObjectPool(Component1)",
+      prepare: ctx => {
+        ctx.pool = new ObjectPool(Component3);
+      },
+      execute: ctx => {
+        for (let i = 0; i < 100000; i++) {
+          ctx.pool.acquire();
+        }
+      }
+    })
+    .add({
+      name: "returning 100k. ObjectPool(Component1)",
+      prepare: ctx => {
+        ctx.pool = new ObjectPool(Component3);
+        ctx.components = [];
+        for (let i = 0; i < 100000; i++) {
+          ctx.components.push(ctx.pool.acquire());
+        }
+      },
+      execute: ctx => {
+        for (let i = 0; i < 100000; i++) {
+          ctx.pool.release(ctx.components[i]);
+        }
+      }
     });
 }
