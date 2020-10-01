f117cc9f255862c0fafe6ca052758118e175ccb2
Brad Menchl <brad.menchl@onepeloton.com>
2020-10-01 14:05:54 -0400

    Improve Component types to allow for type inference on schema properties

--- src/Component.d.ts
+++ src/Component.d.ts
@@ -1,9 +1,5 @@
 import { PropType } from "./Types";
 
-/**
- * Base class for components.
- */
-
 export type ComponentSchemaProp = {
   default?: any;
   type: PropType<any, any>;
@@ -13,18 +9,22 @@ export type ComponentSchema = {
   [propName: string]: ComponentSchemaProp;
 };
 
-export class Component<C> {
+// Base class for components,
+declare class BaseComponent<C> {
   static schema: ComponentSchema;
   static isComponent: true;
-  constructor(props?: Partial<Omit<C, keyof Component<any>>> | false);
   copy(source: this): this;
   clone(): this;
   reset(): void;
   dispose(): void;
 }
 
-export interface ComponentConstructor<C extends Component<any>> {
+type Component<T> = BaseComponent<T> & T;
+
+export interface ComponentConstructor {
   schema: ComponentSchema;
   isComponent: true;
-  new (props?: Partial<Omit<C, keyof Component<any>>> | false): C;
+  new <T>(props?: Partial<T> | false): Component<T>;
 }
+
+export declare const Component: ComponentConstructor;
