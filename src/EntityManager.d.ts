47a4dcb3e01152d8f87c6696b16e0ce10c108ad8
David Peicho <david.peicho@gmail.com>
2020-10-07 13:02:27 +0200

    typescript: entity-manager: fix missing return types

--- src/EntityManager.d.ts
+++ src/EntityManager.d.ts
@@ -1,20 +1,18 @@
-import { ObjectPool } from "./ObjectPool.js";
-import QueryManager from "./QueryManager.js";
-import EventDispatcher from "./EventDispatcher.js";
-import { SystemStateComponent } from "./SystemStateComponent.js";
+import { ComponentConstructor } from "./Component.js";
+import { Entity } from "./Entity";
 
 export class EntityManager {
 
   constructor(world);
 
-  getEntityByName(name);
+  getEntityByName(name): Entity | undefined;
 
   /**
    * Create a new entity
    */
-  createEntity(name);
+  createEntity(name): Entity;
 
-  entityAddComponent(entity, Component, values);
+  entityAddComponent(entity, Component, values): void;
 
   /**
    * Remove a component from an entity
@@ -22,43 +20,67 @@ export class EntityManager {
    * @param {*} Component Component to remove from the entity
    * @param {Bool} immediately If you want to remove the component immediately instead of deferred (Default is false)
    */
-  entityRemoveComponent(entity, Component, immediately);
+  entityRemoveComponent(entity, Component, immediately): void;
 
-  _entityRemoveComponentSync(entity, Component, index);
+  _entityRemoveComponentSync(entity, Component, index): void;
 
   /**
    * Remove all the components from an entity
    * @param {Entity} entity Entity from which the components will be removed
    */
-  entityRemoveAllComponents(entity, immediately);
+  entityRemoveAllComponents(entity, immediately): void;
 
   /**
    * Remove the entity from this manager. It will clear also its components
    * @param {Entity} entity Entity to remove from the manager
    * @param {Bool} immediately If you want to remove the component immediately instead of deferred (Default is false)
    */
-  removeEntity(entity, immediately);
+  removeEntity(entity, immediately): void;
 
   /**
    * Remove all entities from this manager
    */
-  removeAllEntities();
+  removeAllEntities(): void;
 
-  processDeferredRemoval();
+  processDeferredRemoval(): void;
 
   /**
    * Get a query based on a list of components
    * @param {Array(Component)} Components List of components that will form the query
    */
-  queryComponents(Components);
+  queryComponents(Components): [ ComponentConstructor<any> ];
 
   /**
    * Return number of entities
    */
-  count();
+  count(): number;
 
   /**
    * Return some stats
    */
-  stats();
+  stats(): Stats;
+}
+
+interface EventDispatcherStats {
+  fired: number;
+  handled: number;
+}
+
+interface PoolStats {
+  used: number;
+  size: number;
+}
+
+interface QueryStats {
+  numComponents: number;
+  numEntities: number;
+}
+
+interface Stats {
+  numEntities: number;
+  numQueries: number;
+  queries: { [ key: string ]: QueryStats };
+  numComponentPool: number;
+  componentPool: { [ key: string ]: PoolStats };
+  eventDispatcher: EventDispatcherStats;
 }
